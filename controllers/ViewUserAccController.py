a742c8252acd08525c79b24af44111682600f195
Javier <javierssh@outlook.com>
2025-11-07 00:36:02 +0800

    Edited and separated UserAccount view/search functions, added favicon for easier identification

--- controllers/ViewUserAccController.py
+++ controllers/ViewUserAccController.py
@@ -1,8 +1,11 @@
-from entities.UserAdmin import UserAdmin
+from entities.UserAccount import UserAccount
 
 class ViewUserAccController:
     def __init__(self):
-        self.user_admin = UserAdmin()
+        self.user_account = UserAccount()
 
     def viewUser(self):
-        return self.user_admin.viewUser()
+        return self.user_account.viewUser()
+
+    def searchUser(self, keyword):
+        return self.user_account.searchUser(keyword)

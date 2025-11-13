6571140a5ef80e03eda1783fddf97af268afe1e9
Javier <javierssh@outlook.com>
2025-11-13 17:38:01 +0800

    Deleted unnecessary entities

--- entities/CsrRep.py
+++
@@ -1,9 +0,0 @@
-from database import database_management
-import sqlite3
-import re
-
-class CsrRep:
-    def __init__(self):
-        pass
-    
-    
--- entities/PersonInNeed.py
+++
@@ -1,9 +0,0 @@
-from database import database_management
-import sqlite3
-
-class PersonInNeed:
-    def __init__(self):
-        pass
-
-
-    
